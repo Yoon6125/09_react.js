97e30eddb09fb1b36bebbac56830e90635043ab3
Heegeun.Yoon <58425997+Yoon6125@users.noreply.github.com>
2024-11-13 17:51:25 +0900

    Add files via upload

---
+++ react + Spring 으로 카드 가챠 시스템 구현 연습/my_cat_card_my.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_cat
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `card_my`
+--
+
+DROP TABLE IF EXISTS `card_my`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `card_my` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `job` char(20) NOT NULL DEFAULT '전사',
+  `grade` char(20) NOT NULL DEFAULT 'N',
+  `id` char(20) NOT NULL DEFAULT 'cat',
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card_my`
+--
+
+LOCK TABLES `card_my` WRITE;
+/*!40000 ALTER TABLE `card_my` DISABLE KEYS */;
+INSERT INTO `card_my` VALUES (1,'전사','N','cat'),(2,'전사','N','cat'),(3,'전사','N','cat'),(4,'사제','N','cat'),(5,'사제','SSR','cat'),(6,'사제','R','cat'),(7,'마법사','H','cat'),(8,'전사','N','cat');
+/*!40000 ALTER TABLE `card_my` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-13 17:50:51
